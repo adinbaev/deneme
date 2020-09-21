d85cef0870ae88a677f69ee96adf6f26a44ffcbd
Raim <bingo.9505@gmail.com>
2020-09-21 22:14:42 +0300

    enson yapılan değişiklik

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/*yeni branc */}
+    {/*masterda yapılan değişiklik*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
