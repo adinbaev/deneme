13266a4675f1962f24d267d8a0cc6854a7194c47
Raim <bingo.9505@gmail.com>
2020-09-21 21:57:21 +0300

    yorum satiri eklendi

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* sdşlasdşlasad */}
   </React.StrictMode>,
   document.getElementById('root')
 );
